6a33f3661bd03793ec4311919685286ffe27ad03
Natalia Graczyk <natka.graczyk@gmail.com>
2022-12-29 18:34:39 +0100

    overwrite error 404 opage

--- src/main/java/pl/graczyk/trainingassistant/web/ExerciseController.java
+++ src/main/java/pl/graczyk/trainingassistant/web/ExerciseController.java
@@ -1,10 +1,12 @@
 package pl.graczyk.trainingassistant.web;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 import pl.graczyk.trainingassistant.domain.exercise.ExerciseService;
 import pl.graczyk.trainingassistant.domain.exercise.dto.ExerciseDto;
 
@@ -19,8 +21,9 @@ public class ExerciseController {
     }
     @GetMapping("/cwiczenia/{id}")
     public String getExercise(@PathVariable long id, Model model) {
-        Optional<ExerciseDto> optionalExercise = exerciseService.findExerciseById(id);
-        optionalExercise.ifPresent(exercise -> model.addAttribute("exercise", exercise));
-    return "exercise";
+        ExerciseDto exercise = exerciseService.findExerciseById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        model.addAttribute("exercise", exercise);
+        return "exercise";
     }
 }

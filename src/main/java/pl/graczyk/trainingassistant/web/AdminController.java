3e01dd4730181a41a95f6826c534622a230475e5
Natalia Graczyk <natka.graczyk@gmail.com>
2022-12-30 15:56:12 +0100

    create administration panel

---
+++ src/main/java/pl/graczyk/trainingassistant/web/AdminController.java
@@ -0,0 +1,13 @@
+package pl.graczyk.trainingassistant.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @GetMapping("/admin")
+    public String getAdminPanel(){
+        return "admin/admin";
+    }
+
+}

c51cf11160ce55bc89aa33b9dfdf687238bdfaa8
Natalia Graczyk <natka.graczyk@gmail.com>
2023-01-04 12:58:32 +0100

    clean html layout

---
+++ src/test/java/pl/graczyk/trainingassistant/TrainingassistantApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.graczyk.trainingassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingassistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
